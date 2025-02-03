d2d25dc1356084f9a19048e48686a41133afddca
ebookreader24 <116217354+ebookreader24@users.noreply.github.com>
2025-02-03 22:50:19 +0530

    Update index.php

--- Mac0helpS0h0line67/index.php
+++ Mac0helpS0h0line67/index.php
@@ -3,6 +3,13 @@
 
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <head>
+	<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "prucwam4u2");
+</script>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-45WT49YJJJ"></script>
 <script>
